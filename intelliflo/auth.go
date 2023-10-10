2f6f931227eec150c76a4daa56fbc9c0b812bc1f
Jaime Boardman <jaime@karman.digital>
2023-10-10 16:51:10 +0100

    remove debugging logs

--- intelliflo/auth.go
+++ intelliflo/auth.go
@@ -39,7 +39,6 @@ func (c *credentials) GenerateAccessToken(clientSecret string, clientId string, 
 	if resp.StatusCode != http.StatusOK {
 		return tokenBody, fmt.Errorf("error returned by endpoint: %s", tokenRawBody)
 	}
-	fmt.Println(string(tokenRawBody))
 	err = json.Unmarshal(tokenRawBody, &tokenBody)
 	if err != nil {
 		return tokenBody, fmt.Errorf("error parsing body: %s", err)
