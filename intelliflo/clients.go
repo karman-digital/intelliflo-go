d078db0dd2d28f061bb428acef095888da5188b4
Jaime Boardman <jaime@karman.digital>
2023-11-03 11:57:52 +0000

    add function for updating client

--- intelliflo/clients.go
+++ intelliflo/clients.go
@@ -118,3 +118,35 @@ func (c *credentials) GetClient(clientId int) (intelliflomodels.Client, error) {
 	}
 	return client, nil
 }
+
+func (c *credentials) PutClient(clientId int, client intelliflomodels.Client) (intelliflomodels.Client, error) {
+	var updatedClient intelliflomodels.Client
+	reqBody, err := json.Marshal(client)
+	if err != nil {
+		return updatedClient, fmt.Errorf("error converting to body: %v", err)
+	}
+	req, err := retryablehttp.NewRequest("PUT", fmt.Sprintf("https://api.gb.intelliflo.net/v2/clients/%d", clientId), reqBody)
+	if err != nil {
+		return updatedClient, fmt.Errorf("error creating request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header["x-api-key"] = []string{c.apiKey.String()}
+	req.Header["authorization"] = []string{fmt.Sprintf("Bearer %s", c.accessToken)}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return updatedClient, fmt.Errorf("error making post request: %v", err)
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return updatedClient, fmt.Errorf("error reading body: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return updatedClient, fmt.Errorf("error returned by endpoint, status code: %d, body: %s", resp.StatusCode, respBody)
+	}
+	err = json.Unmarshal(respBody, &updatedClient)
+	if err != nil {
+		return updatedClient, fmt.Errorf("error parsing body: %v", err)
+	}
+	return updatedClient, nil
+}
