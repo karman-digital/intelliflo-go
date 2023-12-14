f173e1cdae3fc4bca5f012e0bb937162d222480c
Jaime Boardman <jaime@karman.digital>
2023-12-14 15:45:54 +0000

    Set struct fields in relationship to pointers to allow zero value

--- models/relationship.go
+++ models/relationship.go
@@ -1,16 +1,16 @@
 package intelliflomodels
 
 type Relationship struct {
-	ID                             int              `json:"id,omitempty"`
-	Href                           string           `json:"href,omitempty"`
-	RelationshipType               RelationshipType `json:"relationshipType"`
-	Subject                        RelationSubject  `json:"subject"`
-	Relation                       RelationSubject  `json:"relation"`
-	IsFinancialPartnership         *bool            `json:"isFinancialPartnership,omitempty"`
-	IsFamilyGroup                  *bool            `json:"isFamilyGroup,omitempty"`
-	IncludeInRelationsFamilyWealth *bool            `json:"includeInRelationsFamilyWealth,omitempty"`
-	IsPointOfContact               *bool            `json:"isPointOfContact,omitempty"`
-	StartedOn                      string           `json:"startedOn,omitempty"`
+	ID                             int               `json:"id,omitempty"`
+	Href                           string            `json:"href,omitempty"`
+	RelationshipType               *RelationshipType `json:"relationshipType"`
+	Subject                        *RelationSubject  `json:"subject"`
+	Relation                       *RelationSubject  `json:"relation"`
+	IsFinancialPartnership         *bool             `json:"isFinancialPartnership,omitempty"`
+	IsFamilyGroup                  *bool             `json:"isFamilyGroup,omitempty"`
+	IncludeInRelationsFamilyWealth *bool             `json:"includeInRelationsFamilyWealth,omitempty"`
+	IsPointOfContact               *bool             `json:"isPointOfContact,omitempty"`
+	StartedOn                      string            `json:"startedOn,omitempty"`
 }
 
 type RelationshipType struct {
@@ -18,12 +18,13 @@ type RelationshipType struct {
 }
 
 type RelationSubject struct {
-	ID                  int    `json:"id"`
-	Type                string `json:"type"`
-	Href                string `json:"href,omitempty"`
-	IsHeadOfFamilyGroup bool   `json:"isHeadOfFamilyGroup,omitempty"`
-	PartyType           string `json:"partyType,omitempty"`
-	AccessToRelation    Access `json:"accessToRelation,omitempty"`
+	ID                  int     `json:"id"`
+	Type                string  `json:"type"`
+	Href                string  `json:"href,omitempty"`
+	IsHeadOfFamilyGroup *bool   `json:"isHeadOfFamilyGroup,omitempty"`
+	PartyType           string  `json:"partyType,omitempty"`
+	AccessToRelation    *Access `json:"accessToRelation,omitempty"`
+	AccessToSubject     *Access `json:"accessToSubject,omitempty"`
 }
 
 type Access struct {
