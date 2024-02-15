d2be0fa1b7f17ce313994497dd3e768d530f92da
Jaime Boardman <jaime@karman.digital>
2024-02-15 13:09:32 +0000

    test update to error reporting in call

--- intelliflo/api/init.go
+++ intelliflo/api/init.go
@@ -1,10 +1,15 @@
 package intelliflo
 
-import "github.com/hashicorp/go-retryablehttp"
+import (
+	"log"
+	"os"
+
+	"github.com/hashicorp/go-retryablehttp"
+)
 
 func InitIntellifloAPI() IntellfloAPI {
 	client := retryablehttp.NewClient()
-	client.Logger = nil
+	client.Logger = log.New(os.Stderr, "", log.LstdFlags)
 	return &credentials{
 		client: client,
 	}
