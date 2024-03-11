fbc55be9d588e64bf07e7fae43e340baf55dfa5d
Jaime Boardman <jaime@karman.digital>
2024-03-11 21:10:05 +0000

    remove debug log and add errorHandler return

--- intelliflo/api/init.go
+++ intelliflo/api/init.go
@@ -1,15 +1,17 @@
 package intelliflo
 
 import (
-	"log"
-	"os"
+	"net/http"
 
 	"github.com/hashicorp/go-retryablehttp"
 )
 
 func InitIntellifloAPI() IntellfloAPI {
 	client := retryablehttp.NewClient()
-	client.Logger = log.New(os.Stderr, "", log.LstdFlags)
+	client.Logger = nil
+	client.ErrorHandler = func(resp *http.Response, err error, numTries int) (*http.Response, error) {
+		return resp, nil
+	}
 	return &credentials{
 		client: client,
 	}
