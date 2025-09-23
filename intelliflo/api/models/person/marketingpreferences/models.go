194dcfd6aac391a8fe8be85c54004d42a08d93db
Jaime Boardman <jaime@karman.digital>
2025-09-23 15:31:18 +0100

    add NullableDateTime type for proper handling of null/empty date values in marketing preferences

--- intelliflo/api/models/person/marketingpreferences/models.go
+++ intelliflo/api/models/person/marketingpreferences/models.go
@@ -1,6 +1,8 @@
 package marketingpreferencemodels
 
-import "time"
+import (
+	sharedmodels "github.com/karman-digital/intelliflo-go/intelliflo/api/models/shared"
+)
 
 type PrefClient struct {
 	ID   int32  `json:"id,omitempty"`
@@ -8,25 +10,25 @@ type PrefClient struct {
 }
 
 type Preferences struct {
-	Href                                   string     `json:"href,omitempty"`
-	Client                                 PrefClient `json:"client"`
-	ID                                     int64      `json:"id,omitempty"`
-	AllowCompanyContactByTelephone         bool       `json:"allowCompanyContactByTelephone"`
-	AllowCompanyContactByMail              bool       `json:"allowCompanyContactByMail"`
-	AllowCompanyContactByEmail             bool       `json:"allowCompanyContactByEmail"`
-	AllowCompanyContactBySms               bool       `json:"allowCompanyContactBySms"`
-	AllowCompanyContactBySocialMedia       bool       `json:"allowCompanyContactBySocialMedia"`
-	AllowCompanyContactByAutomatedCalls    bool       `json:"allowCompanyContactByAutomatedCalls"`
-	AllowCompanyContactByPfp               bool       `json:"allowCompanyContactByPfp"`
-	AllowThirdPartyContactByTelephone      bool       `json:"allowThirdPartyContactByTelephone"`
-	AllowThirdPartyContactByMail           bool       `json:"allowThirdPartyContactByMail"`
-	AllowThirdPartyContactByEmail          bool       `json:"allowThirdPartyContactByEmail"`
-	AllowThirdPartyContactBySms            bool       `json:"allowThirdPartyContactBySms"`
-	AllowThirdPartyContactBySocialMedia    bool       `json:"allowThirdPartyContactBySocialMedia"`
-	AllowThirdPartyContactByAutomatedCalls bool       `json:"allowThirdPartyContactByAutomatedCalls"`
-	AllowThirdPartyContactByPfp            bool       `json:"allowThirdPartyContactByPfp"`
-	CanContactForMarketingPurposes         string     `json:"canContactForMarketingPurposes"`
-	ConsentedAt                            time.Time  `json:"consentedAt,omitempty"`
-	DeliveryMethod                         string     `json:"deliveryMethod,omitempty"`
-	AccessibleFormat                       string     `json:"accessibleFormat,omitempty"`
+	Href                                   string                        `json:"href,omitempty"`
+	Client                                 PrefClient                    `json:"client"`
+	ID                                     int64                         `json:"id,omitempty"`
+	AllowCompanyContactByTelephone         bool                          `json:"allowCompanyContactByTelephone"`
+	AllowCompanyContactByMail              bool                          `json:"allowCompanyContactByMail"`
+	AllowCompanyContactByEmail             bool                          `json:"allowCompanyContactByEmail"`
+	AllowCompanyContactBySms               bool                          `json:"allowCompanyContactBySms"`
+	AllowCompanyContactBySocialMedia       bool                          `json:"allowCompanyContactBySocialMedia"`
+	AllowCompanyContactByAutomatedCalls    bool                          `json:"allowCompanyContactByAutomatedCalls"`
+	AllowCompanyContactByPfp               bool                          `json:"allowCompanyContactByPfp"`
+	AllowThirdPartyContactByTelephone      bool                          `json:"allowThirdPartyContactByTelephone"`
+	AllowThirdPartyContactByMail           bool                          `json:"allowThirdPartyContactByMail"`
+	AllowThirdPartyContactByEmail          bool                          `json:"allowThirdPartyContactByEmail"`
+	AllowThirdPartyContactBySms            bool                          `json:"allowThirdPartyContactBySms"`
+	AllowThirdPartyContactBySocialMedia    bool                          `json:"allowThirdPartyContactBySocialMedia"`
+	AllowThirdPartyContactByAutomatedCalls bool                          `json:"allowThirdPartyContactByAutomatedCalls"`
+	AllowThirdPartyContactByPfp            bool                          `json:"allowThirdPartyContactByPfp"`
+	CanContactForMarketingPurposes         string                        `json:"canContactForMarketingPurposes"`
+	ConsentedAt                            sharedmodels.NullableDateTime `json:"consentedAt,omitempty"`
+	DeliveryMethod                         string                        `json:"deliveryMethod,omitempty"`
+	AccessibleFormat                       string                        `json:"accessibleFormat,omitempty"`
 }
